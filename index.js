fef61e1b655f96802cfe85e5a206df53055198bb
RS550 <149615895+RS550@users.noreply.github.com>
2026-06-15 13:22:24 -0400

    Cleaned up and ready to go
    Fixed decay problem, cleaned up code and added comments. Removed unused visuals from package

--- index.js
+++ index.js
@@ -1,75 +1,78 @@
-const myPet = new Pet("Bluberry");
+const myPet = new Pet("Bluberry");          //Only one pet made 
+document.getElementById("petName").querySelector("h1").textContent = myPet.name;       //Sets title to pet name               //Only pet created
 
+//When called updates visuals on the UI
 function updateUI() {
-    const petImg = document.getElementById("petView");
-    const petName = document.getElementById('petName');
+    const petImg = document.getElementById("petView");  //in scope tag, 'petview' = element responsible for which pet image is displayed
 
-    if(!myPet.isGood()){
-        if(myPet.isSad()){
-            petImg.src = 'sadWBlur.gif';
-        }
-        else if (myPet.isSleepy()){
-            petImg.src = 'sleeping.gif';
-        }
-        else if(myPet.isDead()){
-            petImg.src = 'deadd.gif';
-            document.getElementById("petName").textContent = "You Died";
-        }
-    }
-    else{
+    let curStatus = myPet.statusCheck();    //checks current pet status
+
+    //based on pet status, sets petView image
+        // 0 = no negative status effects
+    if(curStatus == 0) {
         petImg.src = 'idle.gif';
     }
+        // 1 = pet dead, game resets score and changes pet title
+    else if (curStatus == 1) {
+        petImg.src = 'deadd.gif';
+        document.getElementById("petName").querySelector("h1").textContent = myPet.name + ' Died';
+        myPet.resetPet();
+        
+    }
+        // 2 = pet sleeping, changes image
+    else if (curStatus == 2) {petImg.src = 'sleepingZZ.gif';}
+        // 3 = pet sad, changes image
+    else if (curStatus == 3) {petImg.src = 'sadWBlur.gif';}
 
-    document.getElementById("petName").querySelector("h1").textContent = myPet.name;
-    
+    //Update status bars based on current stat values
     document.getElementById("hpBar").style.width = myPet.getHP() + "%";
     document.getElementById("xpBar").style.width = myPet.getXP() + "%";
     document.getElementById("apBar").style.width = myPet.getAP() + "%";
 
-  
-    
+    //Updates posted score    
     document.getElementById("scoreVal").textContent = myPet.getScore();
 }
 
+
+//Button Linked Actions
+    //Feed Button Response
 function feedPet(){
     myPet.addHP();
-    myPet.increaseScore();
-    updateUI();
+    myPet.increaseScore();  //Add points to myPet.HP and myPet.Score
+    updateUI();             //Call updateUI to show changes
 }
-
+    //Sleep Button Response
 function sleepPet(){
     myPet.addXP();
-    myPet.increaseScore();
-    updateUI();
+    myPet.increaseScore();  //Add points to myPet.XP and myPet.Score
+    updateUI();             //Call updateUI to show changes
 }
-
+    //Pet Button Response
 function petPet(){
     myPet.addAP();
-    myPet.increaseScore();
-    updateUI();
+    myPet.increaseScore();  //Add points to myPet.AP and myPet.Score
+    updateUI();             //Call updateUI to show changes
 }
 
-function petStats(){
-    if(!myPet.isGood){
-        myPet.decreaseScore();
-    }
-}
-
+//After first button pressed score automatically increases over 0
+    //starts status decay, setInterval() calls a function at the specified interval
 function startDecay(){
     setInterval(function() {
         if(myPet.getScore() > 0){
-            petStats();
             myPet.decay();
             updateUI();
         }
-    }, 700);
+    }, 7000);    //Interval number represents milliseconds
 }
 
-
+//Event Listener for document object
 document.addEventListener("DOMContentLoaded", function() {
+        // updates UI to match current pet status
     updateUI();
+        //when HTML buttons are pressed, the associated function is called
     document.getElementById("feedB").addEventListener("click", feedPet);
     document.getElementById("sleepB").addEventListener("click", sleepPet);
     document.getElementById("petB").addEventListener("click", petPet);
+        //calls startDecay to set decrementing interval 
     startDecay();
 });
--- pet.js
+++ pet.js
@@ -2,46 +2,85 @@ class Pet {
     constructor(name) {
         this.name = name;
         this.hp = 100;
-        this.xp = 100;
+        this.xp = 75;
         this.ap = 50;
         this.score = 0;
     }
- 
+
     increaseScore() { this.score += 10; }
 
+    //check status decrease Score based on negative status effect
     decreaseScore(){ 
-        if(this.isSleepy() || this.isSad()){
-            this.score -= 20;
-        }else if(this.isDead()){this.score == 0;}
+        
+        let curStatus = statusCheck();  // Status Values 0 = good, 1 = dead, 2 = sleepy, 3 = sad
+
+        if(curStatus >= 2){
+            this.score -= 20;           // Decrease points only for sleepy and sad
+        }else if(curStatus == 1){
+            this.score == 0;            // Game over for dead
+        }
+    }
+
+    //On 0 HP game resets 
+    resetPet() {
+        this.hp = 100;
+        this.xp = 75;
+        this.ap = 75;
+        this.score = 0;
     }
  
-    addHP() { this.hp = Math.min(100, this.hp + 10); }
-    addXP() { this.xp = Math.min(100, this.xp + 10); }
-    addAP() { this.ap = Math.min(100, this.ap + 10); }
+    //increases specified stat
+    addHP() { this.hp = Math.min(100, this.hp + 10);}
+    addXP() { this.xp = Math.min(100, this.xp + 10);}
+    addAP() { this.ap = Math.min(100, this.ap + 10);}
  
-    subHP() { this.hp = Math.max(0, this.hp - 10); }
-    subXP() { this.xp = Math.max(0, this.xp - 10); }
-    subAP() { this.ap = Math.max(0, this.ap - 10); }
+    //decreases specified stat
+    subHP() { this.hp = Math.max(0, this.hp - 10);}
+    subXP() { this.xp = Math.max(0, this.xp - 10);}
+    subAP() { this.ap = Math.max(0, this.ap - 10);}
  
     getScore() { return this.score; }
     getHP()    { return this.hp; }
     getXP()    { return this.xp; }
     getAP()    { return this.ap; }
 
+    //selects status to decrease
     decay(){
         let selection = Math.floor(Math.random() * 4);
-        if(selection >= 3){this.subHP();}
+        if(selection == 3){this.subHP();}
         else if(selection == 2){this.subXP();}
-        else {this.subAP();}
+        else if(selection == 1){this.subAP();}
+        else {
+            this.subHP();
+            this.subXP();
+            this.subAP();
+        }
     }
 
-    isDead(){ return this.hp<= 0; }
+    //calls to determine if pet has current negative status effect or dead
+    isDead(){ 
+        if(this.hp <= 0){
+            return true;
+        }else {return false;} 
+    }
+    isSleepy(){
+        if(this.xp <= 0){
+            return true;
+        }else {return false;} 
+    }
+    isSad(){
+        if(this.ap <= 0){
+            return true;
+        }else {return false;} 
+    }
 
-    isSleepy(){ return this.xp <= 0; }
-
-    isSad(){ return this.ap <= 0; }
-
-    isGood(){ return this.hp > 0 && this.xp > 0 && this.ap > 0; }
+    //Check current pet status, calls above to check for negative status effects
+    statusCheck(){ 
+        if(this.isDead()){return 1;}
+        else if (this.isSleepy()){return 2;}
+        else if (this.isSad()){return 3;}
+        else {return 0};
+ }
 
 }
 
